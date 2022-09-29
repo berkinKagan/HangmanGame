70b862c7262961b08358d2b761b1d986308a2417
Burak Baştuğ <99401604+BurakBastug@users.noreply.github.com>
2022-09-29 13:33:43 +0300

    Veriables and some getters added

--- Hangman.java
+++ Hangman.java
@@ -1,1 +1,32 @@
+/**
+ * Hangman
+ */
+public class Hangman {
 
+    private StringBuffer secretWord;
+    private StringBuffer allLetters;
+    private StringBuffer usedLetters;
+    private int numberOfIncorrectTries;
+    private int maxAllowedIncorrectTries;
+    private StringBuffer knownSoFar;
+
+    public StringBuffer getAllLetters() {
+        return allLetters;
+    }
+
+    public StringBuffer getUsedLetters() {
+        return usedLetters;
+    }
+
+    public int getNumOfIncorrectTries() {
+        return numberOfIncorrectTries;
+    }
+
+    public int getMaxAllowedIncorrectTries() {
+        return maxAllowedIncorrectTries;
+    }  
+    
+
+    
+
+}
