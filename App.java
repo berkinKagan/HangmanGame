86166835c0441f3055e1627e5a70515b471a3eff
Arda <ardaoztrk@yahoo.com>
2022-09-30 23:57:25 +0300

    hangman algoritması ve birkaç metod eklendi

--- App.java
+++ App.java
@@ -2,7 +2,8 @@
 public class App{
     public static void main(String[] args) {
         //JPanel panel = new JPanel();
-        
+        //  Hangman hangman = new Hangman(); 
+        //  hangman.playGame();
         
         new Myframe();
 
--- Hangman.java
+++ Hangman.java
@@ -2,42 +2,126 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/**
- * Hangman
- */
 public class Hangman {
-
-    private StringBuffer secretWord = new StringBuffer();
-    private StringBuffer allLetters = new StringBuffer();
-    private StringBuffer usedLetters = new StringBuffer();
+    private StringBuffer secretWord;
+    private StringBuffer allLetters;
+    private StringBuffer usedLetters;
+    private StringBuffer knownSoFar;
+    private String[] wordList = new String[20]; 
     private int numberOfIncorrectTries;
     private int maxAllowedIncorrectTries;
-    private StringBuffer knownSoFar = new StringBuffer();
-    private String[] wordList = new String[20];
+    private int MAX_INCORRECT_TRIES = 6;
 
-    
     public Hangman() throws FileNotFoundException {
-        maxAllowedIncorrectTries=6;
-        chooseSecretWord();
+        secretWord = chooseSecretWord();
+        maxAllowedIncorrectTries = MAX_INCORRECT_TRIES;
+        initKnownSoFar();
     }
+
+    public void playGame() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("The word is:");
+        System.out.println(this.getKnownSoFar() + "\n");
+
+        while(!this.isGameOver()) {
+            System.out.println("Enter a (1)letter or (2)the word to guess");
+            int choice = input.nextInt();
+            while(choice<1 || choice>2) {
+                choice = input.nextInt();
+            }
+            if(choice == 1) {
+                System.out.println("Enter a letter:");
+                char ch = input.next().charAt(0);
+
+                System.out.println("The letter has occurred " + this.tryThis(ch) + " times.");
+                System.out.println("Known so far is:");
+                this.revealKnownLetters(ch);
+                System.out.println(this.getKnownSoFar());
     
-    /**
-     * it initializes words array from the text file including superhero names and chooses random word from the array
-     * @return secret word (as stringbuffer)
-     * @throws FileNotFoundException
-     */   
-    private StringBuffer chooseSecretWord() throws FileNotFoundException {        
+                if(this.tryThis(ch) == 0) {
+                    this.incrementNumberOfIncorrectTries();
+                }
+            }
+            else if(choice == 2) {
+                System.out.println("Enter your guess:");
+                String guess = input.next();
+                if(this.tryThis(guess) == 0) {
+                    this.incrementNumberOfIncorrectTries();
+                }
+                else {
+                    //kelimenin tamamı denencek
+                }
+            }
+        }
+        if(this.hasLost()) {
+            System.out.println("You Lost!");
+        }
+        else{
+            System.out.println("You Won!");
+        }
+        input.close();
+    }
+
+    public void initKnownSoFar() {
+        this.knownSoFar = new StringBuffer();
+        for(int i=0; i<this.secretWord.length(); i++) {
+            this.knownSoFar = this.knownSoFar.append("_");
+        }
+    }
+
+    public void revealKnownLetters(char ch) {
+        for(int i=0; i<this.secretWord.length(); i++) {
+            if(this.secretWord.charAt(i) == ch) {
+                this.knownSoFar.setCharAt(i, ch);
+            }
+        }
+    }
+
+    public int tryThis(String str) {
+        if(this.secretWord.toString().equals(this.knownSoFar.toString())) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public int tryThis(char ch) {
+        int count = 0;
+        for(int i=0; i<this.secretWord.length(); i++) {
+            if(this.secretWord.charAt(i) == ch) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isGameOver() {
+        return numberOfIncorrectTries > MAX_INCORRECT_TRIES || this.secretWord.toString().equals(this.knownSoFar.toString()); 
+    }
+
+    public boolean hasLost() {
+        return numberOfIncorrectTries > MAX_INCORRECT_TRIES;
+    }
+
+    public void incrementNumberOfIncorrectTries() {
+        this.numberOfIncorrectTries++;
+    }
+
+    private StringBuffer chooseSecretWord() throws FileNotFoundException {
+        secretWord = new StringBuffer();
         Scanner fileScan = new Scanner(new File("hangman_Words_List.txt"));
         int i = 0;        
         while (fileScan.hasNext()) {
             wordList[i]=fileScan.nextLine();
             i++;
         }
-        secretWord = secretWord.append(wordList[(int) (Math.random()*20)]);        
+        secretWord = secretWord.append(wordList[(int) (Math.random()*21)]);        
         fileScan.close();
         return secretWord;
-    }    
-    
+    }
+
     public StringBuffer getAllLetters() {
         return allLetters;
     }
@@ -52,5 +136,13 @@ public class Hangman {
 
     public int getMaxAllowedIncorrectTries() {
         return maxAllowedIncorrectTries;
-    }     
+    }    
+
+    public StringBuffer getKnownSoFar() {
+        return knownSoFar;
+    }
+
+    public void setUsedLetters(char ch) {
+        usedLetters = this.usedLetters.append(ch);
+    }
 }
